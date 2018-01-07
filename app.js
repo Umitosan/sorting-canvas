bd45dd29772c2fd5a7faf266bbea8957da831d0b
Dominic Brown <yeahdom@gmail.com>
2018-01-07 03:18:47 -0800

    add clear function / button

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 
-
 function draw() {
-
   var canvas = document.getElementById('canvas');
-
   if (canvas.getContext) {
 
     var ctx = canvas.getContext('2d');
@@ -18,21 +15,39 @@ function draw() {
 
     // fillRect(x, y, width, height)
 
-    for (var i = 0; i < 5 ;i++) {
+    for (var i = 0; i < 10 ;i++) {
+      // alternate between color boxes
       if ((i % 2) === 0) {
         ctx.fillStyle = ctx1;
       } else {
         ctx.fillStyle = ctx2;
       }
+      // color the box in
       ctx.fillRect(x+(i*20), y+(i*20), width, height);
     } // end for
-
   } // end if
-
 } // end draw
+
+
+function clearCanvas() {
+  // var canvas = document.getElementById('canvas');
+  var canvas = $('#canvas')[0];
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+}
 
 
 $(document).ready(function(){
   console.log("Let the drawing begin!");
+
+  // initial draw
   draw();
+
+  $('.btn1').click(function() {
+    draw();
+  });
+
+  $('.btn2').click(function() {
+    clearCanvas();
+  });
+
 });
--- notes.js
+++ notes.js
@@ -1,5 +1,30 @@
 // Canvas notes
 
+
+// basic shapes
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
+
+// 1. First, you create the path.
+// 2. Then you use drawing commands to draw into the path.
+// 3. Once the path has been created, you can stroke or fill the path to render it.
+
+beginPath()
+// Creates a new path. Once created, future drawing commands are directed into the path and used to build the path up.
+// Path methods
+// Methods to set different paths for objects.
+
+closePath()
+// Adds a straight line to the path, going to the start of the current sub-path.
+
+stroke()
+// Draws the shape by stroking its outline.
+
+fill()
+// Draws a solid shape by filling the path's content area.
+
+
+
+
 // Draws a filled rectangle.
 fillRect(x, y, width, height)
 
