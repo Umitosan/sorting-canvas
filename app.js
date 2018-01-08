69bcb9127fe6431dbc07f5a9fc897d6e43932253
Dominic Brown <yeahdom@gmail.com>
2018-01-07 20:03:13 -0800

    add box move, color changes

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 
+// see this for html names colors
+// https://www.w3schools.com/colors/colors_shades.asp
 
-var fBlack = 'rgba(0, 0, 0, 1)'
+var fBlack = 'rgba(0, 0, 0, 1)';
+var fdarkGrey = 'rgba(50, 50, 50, 1)';
+var flightGreyTrans = 'rgba(50, 50, 50, 0.3)';
+var flightGreySolid = 'rgb(211,211,211)';
+var fWhite = 'rgba(250, 250, 250, 1)';
 var fRed = 'rgba(200, 0, 0, 0.5)';
 var fGreen = 'rgba(0, 200, 0, 0.5)';
 var fBlue = 'rgba(0, 0, 200, 0.5)';
 
+var canvasWidth = 800;
+
 var x1 = 10;
 var y1 = 10;
-var x3 = 5;
-var y3 = 5;
+var x3 = 20;
+var y3 = 150;
+var xVel3 = 10;
 var boxWidth = 50;
 var boxHeight = 50;
 
@@ -20,7 +29,7 @@ function draw1() {
 
     var ctx = canvas.getContext('2d');
 
-    for (var i = 0; i < 10 ;i++) {
+    for (var i = 0; i < 35 ;i++) {
       // alternate between color boxes
       if ( ((i % 2) === 0) && ((i % 3) !== 0) ) {
         ctx.fillStyle = fBlue;
@@ -30,7 +39,7 @@ function draw1() {
         ctx.fillStyle = fRed;
       }
       // fillRect(x, y, width, height)
-      ctx.fillRect(x1+(i*20), y1+(i*20), boxWidth, boxHeight);
+      ctx.fillRect(x1+(i*20), y1+(i*10), boxWidth, boxHeight);
     } // end for
   } // end if
 } // end draw
@@ -38,11 +47,19 @@ function draw1() {
 function draw3() {
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
-    if (x3 < 500) {
-      x3 += 10;
-    }
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = fBlack;
+    // clear old box
+    ctx.fillStyle = flightGreySolid;
+    ctx.fillRect(x3, y3, boxWidth, boxHeight);
+    // change velocity if needed
+    if (x3+10+boxWidth > canvasWidth) {
+      xVel3 = -10;
+    } else if (x3-10 < 0) {
+      xVel3 = 10;
+    }
+    // move box
+    x3 += xVel3;
+    ctx.fillStyle = flightGreyTrans;
     ctx.fillRect(x3, y3, boxWidth, boxHeight);
   }
 }
@@ -75,8 +92,12 @@ $(document).ready(function() {
   });
 
   $('.btn3').click(function() {
-    // draw3init();
-    setInterval(draw3(), 200);
+    setInterval(draw3, 50);
+  });
+
+  $('.btn4').click(function() {
+    x3 = 50;
+    y3 = 150;
   });
 
 });
