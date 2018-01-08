112e714affc57437893b495cb233ca253dbc705f
Dominic Brown <yeahdom@gmail.com>
2018-01-07 20:42:18 -0800

    refactor using a master gameLoop

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 // see this for html names colors
 // https://www.w3schools.com/colors/colors_shades.asp
 
+// all colors to be used with the canvas functions
 var fBlack = 'rgba(0, 0, 0, 1)';
 var fdarkGrey = 'rgba(50, 50, 50, 1)';
 var flightGreyTrans = 'rgba(50, 50, 50, 0.3)';
-var flightGreySolid = 'rgb(211,211,211)';
+var fGreyReset = 'rgb(211,211,211)';
 var fWhite = 'rgba(250, 250, 250, 1)';
 var fRed = 'rgba(200, 0, 0, 0.5)';
 var fGreen = 'rgba(0, 200, 0, 0.5)';
 var fBlue = 'rgba(0, 0, 200, 0.5)';
 
 var canvasWidth = 800;
+// canvas must be defined here for backend funcs
+var canvas = undefined;
 
 var x1 = 10;
 var y1 = 10;
@@ -23,12 +26,20 @@ var boxHeight = 50;
 
 var time = 0;
 
+function gameLoop() {
+  draw1();
+  draw3();
+}
+
+
 function draw1() {
-  var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
-
     var ctx = canvas.getContext('2d');
-
+    // clear old boxes
+    ctx.fillStyle = fGreyReset;
+    for (var i = 0; i < 35 ;i++) {
+      ctx.fillRect(x1+(i*20), y1+(i*10), boxWidth, boxHeight);
+    }
     for (var i = 0; i < 35 ;i++) {
       // alternate between color boxes
       if ( ((i % 2) === 0) && ((i % 3) !== 0) ) {
@@ -38,18 +49,17 @@ function draw1() {
       } else {
         ctx.fillStyle = fRed;
       }
-      // fillRect(x, y, width, height)
       ctx.fillRect(x1+(i*20), y1+(i*10), boxWidth, boxHeight);
     } // end for
   } // end if
 } // end draw
 
+
 function draw3() {
-  var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
     // clear old box
-    ctx.fillStyle = flightGreySolid;
+    ctx.fillStyle = fGreyReset;
     ctx.fillRect(x3, y3, boxWidth, boxHeight);
     // change velocity if needed
     if (x3+10+boxWidth > canvasWidth) {
@@ -64,6 +74,7 @@ function draw3() {
   }
 }
 
+
 function clearCanvas() {
   // var canvas = document.getElementById('canvas');
   var canvas = $('#canvas')[0];
@@ -71,17 +82,21 @@ function clearCanvas() {
 }
 
 
+function clockTimer() {
+  time += 1;
+  $('#clock').text(time);
+}
+
+
 // FRONT
 // FRONT
 // FRONT
 $(document).ready(function() {
-
-  function clockTimer() {
-    time += 1;
-    $('#clock').text(time);
-  }
+  // canvas is instantiated above to be global
+  canvas = document.getElementById('canvas');
 
   setInterval(clockTimer, 1000);
+  setInterval(gameLoop, 17);
 
   $('.btn1').click(function() {
     draw1();
